d2b372f3987c1d32fea842050b4001cb77d63742
Nolen Johnson <johnsonnolen@gmail.com>
2023-07-29 16:48:04 -0400

    lineage: config: Allow devices without Cameras to opt-out of Aperture
    Change-Id: I6feb538153b13cd1825f37ee7a45ebb046b12ec0

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -5,6 +5,12 @@ $(call inherit-product, vendor/aosp/config/common.mk)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
 
+# Aperture
+ifneq ($(PRODUCT_NO_CAMERA),true)
+PRODUCT_PACKAGES += \
+    Aperture
+endif
+
 # SystemUI plugins
 PRODUCT_PACKAGES += \
     QuickAccessWallet
