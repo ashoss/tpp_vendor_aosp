cf45ee85f3a5d2087f26c2de7b478905c4f489bd
Michael Bestas <mkbestas@lineageos.org>
2024-10-19 01:48:34 +0200

    common: Remove date UTC prop override key
    This has been unused since ICS in 2011.
    
    Change-Id: Ic8779d67778174a7b04f8b79f26b0b3c39556bae

--- config/common.mk
+++ config/common.mk
@@ -12,8 +12,6 @@ endif
 
 PRODUCT_BRAND ?= PixelProject
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=android-google
