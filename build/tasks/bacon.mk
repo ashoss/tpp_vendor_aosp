d304fe19ebd360aba1ea8c289cc182b25c54f71e
sajidshahriar72543 <sazidshahriar39@outlook.com>
2024-11-07 16:01:37 +0000

    vendor/aosp: Bring in the The-Pixel-Project ascii art
    Change-Id: I2e7762d9ba62a059e11abddb0b1d7ed2a208384d

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -24,4 +24,5 @@ bacon: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(CUSTOM_TARGET_PACKAGE)
 	$(hide) $(MD5) $(CUSTOM_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(CUSTOM_TARGET_PACKAGE).md5sum
 	$(hide) ./vendor/aosp/build/tools/createjson.sh $(TARGET_DEVICE) $(PRODUCT_OUT) $(CUSTOM_VERSION).zip
+	$(hide) bash vendor/aosp/build/tools/tpp_ascii.sh
 	@echo "Package Complete: $(CUSTOM_TARGET_PACKAGE)" >&2
---
+++ build/tools/tpp_ascii.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define color codes
+RED='\033[0;31m'       # Red color
+YELLOW='\033[0;33m'    # Yellow color
+ENDCOLOR='\033[0m'     # Reset color to default
+
+# Output the colored banner with the updated text "TPP"
+echo -e "${RED}========================================================================${ENDCOLOR}"
+echo -e "${YELLOW}                           _______ _____    _____                    ${ENDCOLOR}"
+echo -e "${YELLOW}                          |__   __|  __ \  |  __ \                   ${ENDCOLOR}"
+echo -e "${YELLOW}                             | |  | |__) | | |__) |                  ${ENDCOLOR}"
+echo -e "${YELLOW}                             | |  |  ___/  |  ___/                   ${ENDCOLOR}"
+echo -e "${YELLOW}                             | |  | |      | |                       ${ENDCOLOR}"
+echo -e "${YELLOW}                             |_|  |_|      |_|                       ${ENDCOLOR}"
+echo -e "${YELLOW}                                                                     ${ENDCOLOR}"
+echo -e "${RED}========================================================================${ENDCOLOR}"
+
+
