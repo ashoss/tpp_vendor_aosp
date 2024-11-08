c82e9cb00217886e6cf9caf3761783dbe9600819
sajidshahriar72543 <sazidshahriar39@outlook.com>
2024-11-08 12:53:31 +0000

    vendor: version: use CUSTOM_BUILD_DATE instead of BUILD_DATE
    For better clarity
    
    Change-Id: Ic57f41bb4d0dcee120fa80977da04eda70eac30c

--- config/version.mk
+++ config/version.mk
@@ -17,7 +17,7 @@ CUSTOM_VERSION := tpp_$(CUSTOM_DISPLAY_VERSION)-$(CUSTOM_BUILD)-$(CUSTOM_PLATFOR
 CUSTOM_VERSION_PROP := fifteen
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.custom.build.date=$(BUILD_DATE) \
+    ro.custom.build.date=$(CUSTOM_BUILD_DATE) \
     ro.custom.build.version=$(CUSTOM_DISPLAY_VERSION) \
     ro.custom.device=$(CUSTOM_BUILD) \
     ro.custom.fingerprint=$(ROM_FINGERPRINT) \
