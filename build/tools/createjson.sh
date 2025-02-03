5a9ede749e0b5b20385535b973f358d565a0f062
sajidshahriar72543 <sazidshahriar39@gmail.com>
2025-02-03 22:09:50 +0600

    fixup! vendor: FULLY get rid of numerical versioning

--- build/tools/createjson.sh
+++ build/tools/createjson.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2024 The Pixel Project
+# Copyright (C) 2019-2023 crDroid Android Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,90 +15,45 @@
 # limitations under the License.
 #
 
-# $1=TARGET_DEVICE, $2=PRODUCT_OUT, $3=FILE_NAME
-existingOTAjson=./vendor/official_devices/OTA/devices/$1.json
+#$1=TARGET_DEVICE, $2=PRODUCT_OUT, $3=FILE_NAME
 output=$2/$1.json
-version=$4
 
-# cleanup old file
-if [ -f $output ]; then
-        rm $output
+# Cleanup old file
+if [ -f "$output" ]; then
+	rm "$output"
 fi
 
 echo "Generating JSON file data for OTA support..."
 
-if [ -f $existingOTAjson ]; then
-        # get data from already existing device json
-        maintainer=`grep -n "\"maintainer\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
-        oem=`grep -n "\"oem\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
-        device=`grep -n "\"device\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
-        filename=$3
-        version=`echo $version`
-        buildprop=$2/system/build.prop
-        linenr=`grep -n "ro.system.build.date.utc" $buildprop | cut -d':' -f1`
-        timestamp=`sed -n $linenr'p' < $buildprop | cut -d'=' -f2`
-        md5=`md5sum "$2/$3" | cut -d' ' -f1`
-        sha256=`sha256sum "$2/$3" | cut -d' ' -f1`
-        size=`stat -c "%s" "$2/$3"`
-        forum=`grep -n "\"forum\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
-        if [ ! -z "$forum" ]; then
-                forum="https:"$forum
-        fi
-        telegram=`grep -n "\"telegram\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
-        if [ ! -z "$telegram" ]; then
-                telegram="https:"$telegram
-        fi
-        
-
-        echo '{
-        "response": [
-                {
-                        "maintainer": "'$maintainer'",
-                        "oem": "'$oem'",
-                        "device": "'$device'",
-                        "version": "'$version'",
-                        "filename": "'$filename'",
-                        "download": "https://sourceforge.net/projects/pixel-project/files/'$1'/'$3'/download",
-                        "timestamp": '$timestamp',
-                        "md5": "'$md5'",
-                        "sha256": "'$sha256'",
-                        "size": '$size',
-                        "forum": "'$forum'",
-                        "telegram": "'$telegram'"
-                }
-        ]
-}' >> $output
-else
-        filename=$3
-        version=`echo $version`
-        buildprop=$2/system/build.prop
-        linenr=`grep -n "ro.system.build.date.utc" $buildprop | cut -d':' -f1`
-        timestamp=`sed -n $linenr'p' < $buildprop | cut -d'=' -f2`
-        md5=`md5sum "$2/$3" | cut -d' ' -f1`
-        sha256=`sha256sum "$2/$3" | cut -d' ' -f1`
-        size=`stat -c "%s" "$2/$3"`
-
-        echo '{
-        "response": [
-                {
-                        "maintainer": "''",
-                        "oem": "''",
-                        "device": "''",
-                        "version": "'$version'",
-                        "filename": "'$filename'",
-                        "download": "https://sourceforge.net/projects/pixel-project/files/'$1'/'$3'/download",
-                        "timestamp": '$timestamp',
-                        "md5": "'$md5'",
-                        "sha256": "'$sha256'",
-                        "size": '$size',
-                        "forum": "''",
-                        "telegram": "''"
-                }
-        ]
-}' >> $output
-
-        echo 'There is no official support for this device yet'
-        echo 'Apply from https://github.com/The-Pixel-Project/official_devices'
+# Check if filename contains "UNOFFICIAL" and exit if true
+if [[ "$3" == *"UNOFFICIAL"* ]]; then
+    echo "Skipping JSON generation as the build is marked UNOFFICIAL."
+    exit 0
 fi
 
-echo ""
+# Set romtype to OFFICIAL since we’ve confirmed it’s not UNOFFICIAL
+romtype="OFFICIAL"
+
+# Generate JSON data
+buildprop="$2/system/build.prop"
+linenr=`grep -n "ro.build.date.utc" "$buildprop" | cut -d':' -f1`
+datetime=`sed -n "$linenr"p < "$buildprop" | cut -d'=' -f2`
+filename="$3"
+id=`sha256sum "$2/$3" | cut -d' ' -f1`
+size=`stat -c "%s" "$2/$3"`
+
+# Create JSON output
+echo '{
+  "response": [
+    {
+      "datetime": '"$datetime"',
+      "filename": "'"$filename"'",
+      "id": "'"$id"'",
+      "romtype": "'"$romtype"'",
+      "size": '"$size"',
+      "url": "https://sourceforge.net/projects/pixel-project/files/'"$1"'/'"$3"'/download"
+    }
+  ]
+}' >> "$output"
+
+echo "JSON file generated successfully at $output"
