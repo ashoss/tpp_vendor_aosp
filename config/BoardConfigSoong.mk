1d4eea7715135522a41030ee7d49b18fd87be729
borbelyvince <borbely.vince.11@gmail.com>
2025-02-07 18:47:02 +0100

    Revert "config: Changes for libfmjni bp conversion"
    This reverts commit e0d3bd9d9.
    
    Reason for revert: The soong namespaces can be removed after reworking
    libfmjni to use select() with filegroups and header libs for qcom/slsi.
    Broadcom is not used in any recent shipping device and mtk can continue
    using prebuilt for now.
    
    Change-Id: I078b321c7c55c3d7e5cd97e2664208c3d44f513a

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -60,7 +60,6 @@ SOONG_CONFIG_customNvidiaVars += \
 
 SOONG_CONFIG_NAMESPACES += customQcomVars
 SOONG_CONFIG_customQcomVars += \
-    no_fm_firmware \
     qti_vibrator_effect_lib \
     qti_vibrator_use_effect_stream \
     supports_extended_compress_format \
@@ -134,18 +133,3 @@ else
 SOONG_CONFIG_customQcomVars_qcom_display_headers_namespace := $(QCOM_SOONG_NAMESPACE)/display
 endif
 SOONG_CONFIG_customQcomVars_qti_vibrator_effect_lib := $(TARGET_QTI_VIBRATOR_EFFECT_LIB)
-
-# libfmjni
-ifeq ($(BOARD_HAVE_QCOM_FM),true)
-    PRODUCT_SOONG_NAMESPACES += \
-        vendor/qcom/opensource/libfmjni
-else ifeq ($(BOARD_HAVE_BCM_FM),true)
-    PRODUCT_SOONG_NAMESPACES += \
-        hardware/broadcom/fm
-else ifeq ($(BOARD_HAVE_SLSI_FM),true)
-    PRODUCT_SOONG_NAMESPACES += \
-        hardware/samsung_slsi/fm
-else ifneq ($(BOARD_HAVE_MTK_FM),true)
-    PRODUCT_SOONG_NAMESPACES += \
-        packages/apps/FMRadio/jni/fmr
-endif
