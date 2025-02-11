d3647fa76236e6daad039653d5402a5ef09a14dc
sajidshahriar72543 <sazidshahriar39@gmail.com>
2025-02-11 16:26:36 +0000

    vendor: re-enable adb root
    Change-Id: Id54d4f433d00e36aa516fb05991ad2952acf6b10

--- config/common.mk
+++ config/common.mk
@@ -225,17 +225,17 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
 endif
 
 # Root
-# PRODUCT_PACKAGES += \
-#     adb_root
-# ifneq ($(TARGET_BUILD_VARIANT),user)
-# ifeq ($(WITH_SU),true)
-# PRODUCT_PACKAGES += \
-#     su
+PRODUCT_PACKAGES += \
+    adb_root
+ifneq ($(TARGET_BUILD_VARIANT),user)
+ifeq ($(WITH_SU),true)
+PRODUCT_PACKAGES += \
+    su
 
-# PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-#     system/xbin/su
-# endif
-# endif
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/xbin/su
+endif
+endif
 
 # SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
