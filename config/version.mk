aospb-staging/vendor_aosp

vendor: check for signing flag for builds
- Also add a trailing `-signed` string if flag is set to true

Change-Id: Ia578e1e67c14c3e12f85c13b13af3b17ce3b01b5

config/version.mk
@@ -4,7 +4,12 @@ CUSTOM_DATE_DAY := $(shell date -u +%d)
 CUSTOM_DATE_HOUR := $(shell date -u +%H)
 CUSTOM_DATE_MINUTE := $(shell date -u +%M)
 CUSTOM_BUILD_DATE_UTC := $(shell date -d '$(CUSTOM_DATE_YEAR)-$(CUSTOM_DATE_MONTH)-$(CUSTOM_DATE_DAY) $(CUSTOM_DATE_HOUR):$(CUSTOM_DATE_MINUTE) UTC' +%s)
-CUSTOM_BUILD_DATE := $(CUSTOM_DATE_YEAR)$(CUSTOM_DATE_MONTH)$(CUSTOM_DATE_DAY)-$(CUSTOM_DATE_HOUR)$(CUSTOM_DATE_MINUTE)
+CUSTOM_BUILD_DATE := $(CUSTOM_DATE_YEAR)$(CUSTOM_DATE_MONTH)$(CUSTOM_DATE_DAY)
+
+# Signing
+ifeq ($(IS_SIGNED),true)
+-include vendor/lineage-priv/keys/keys.mk
+endif
 
 CUSTOM_PLATFORM_VERSION := 15.0
 
@@ -12,7 +17,7 @@ CUSTOM_BUILDTYPE ?= UNOFFICIAL
 
 CUSTOM_SHOW_VERSION := $(CUSTOM_BUILD)-$(CUSTOM_BUILD_DATE)
 
-CUSTOM_VERSION := aosPB_$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILDTYPE)-$(CUSTOM_BUILD_DATE)
+CUSTOM_VERSION := aosPB_$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD)-$(CUSTOM_BUILDTYPE)-$(CUSTOM_BUILD_DATE)$(if $(IS_SIGNED),-signed)
 CUSTOM_VERSION_PROP := fifteen
 
 PRODUCT_SYSTEM_PROPERTIES += \
@@ -23,6 +28,3 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.custom.showversion=$(CUSTOM_SHOW_VERSION) \
     ro.custom.version=$(CUSTOM_VERSION) \
     ro.modversion=$(CUSTOM_VERSION)
-
-# Signing
--include vendor/lineage-priv/keys/keys.mk

ashoss/tpp_vendor_aosp

config/version.mk
@@ -4,7 +4,12 @@ CUSTOM_DATE_DAY := $(shell date -u +%d)
 CUSTOM_DATE_HOUR := $(shell date -u +%H)
 CUSTOM_DATE_MINUTE := $(shell date -u +%M)
 CUSTOM_BUILD_DATE_UTC := $(shell date -d '$(CUSTOM_DATE_YEAR)-$(CUSTOM_DATE_MONTH)-$(CUSTOM_DATE_DAY) $(CUSTOM_DATE_HOUR):$(CUSTOM_DATE_MINUTE) UTC' +%s)
-CUSTOM_BUILD_DATE := $(CUSTOM_DATE_YEAR)$(CUSTOM_DATE_MONTH)$(CUSTOM_DATE_DAY)-$(CUSTOM_DATE_HOUR)$(CUSTOM_DATE_MINUTE)
+CUSTOM_BUILD_DATE := $(CUSTOM_DATE_YEAR)$(CUSTOM_DATE_MONTH)$(CUSTOM_DATE_DAY)
+
+# Signing
+ifeq ($(IS_SIGNED),true)
+-include vendor/lineage-priv/keys/keys.mk
+endif
 
 CUSTOM_PLATFORM_VERSION := 15.0
 CUSTOM_DISPLAY_VERSION := 1.0
@@ -13,7 +18,7 @@ CUSTOM_BUILDTYPE ?= UNOFFICIAL
 
 CUSTOM_SHOW_VERSION := $(CUSTOM_BUILD)-$(CUSTOM_DISPLAY_VERSION)-$(CUSTOM_BUILD_DATE)
 
-CUSTOM_VERSION := tpp_$(CUSTOM_DISPLAY_VERSION)-$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILDTYPE)-$(CUSTOM_BUILD_DATE)
+CUSTOM_VERSION := tpp_$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD)-$(CUSTOM_BUILDTYPE)-$(CUSTOM_BUILD_DATE)$(if $(IS_SIGNED),-signed)
 CUSTOM_VERSION_PROP := fifteen
 
 PRODUCT_SYSTEM_PROPERTIES += \
@@ -25,6 +30,3 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.custom.showversion=$(CUSTOM_SHOW_VERSION) \
     ro.custom.version=$(CUSTOM_VERSION) \
     ro.modversion=$(CUSTOM_VERSION)
-
-# Signing
--include vendor/lineage-priv/keys/keys.mk