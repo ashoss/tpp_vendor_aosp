aospb-staging/vendor_aosp

vendor: reconfigure OTA
Also remove duplicated unnecessary PRODUCT_SYSTEM_DEFAULT_PROPERTIES section

config/version.mk
@@ -7,6 +7,7 @@ CUSTOM_BUILD_DATE_UTC := $(shell date -d '$(CUSTOM_DATE_YEAR)-$(CUSTOM_DATE_MONT
 CUSTOM_BUILD_DATE := $(CUSTOM_DATE_YEAR)$(CUSTOM_DATE_MONTH)$(CUSTOM_DATE_DAY)-$(CUSTOM_DATE_HOUR)$(CUSTOM_DATE_MINUTE)
 
 CUSTOM_PLATFORM_VERSION := 15.0
+CUSTOM_DISPLAY_VERSION := 1.0
 
 # Guidelines for setting aosPB Build ID:
 #  - VR1  = V Release 1
@@ -19,20 +20,17 @@ CUSTOM_PLATFORM_VERSION := 15.0
 
 CUSTOM_BUILD_ID := VR1.35.DEV.OCT.BETA01
 
-CUSTOM_VERSION := aosPB_$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)
+CUSTOM_BUILDTYPE ?= UNOFFICIAL
+
+CUSTOM_VERSION := aosPB_$(CUSTOM_DISPLAY_VERSION)-$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILDTYPE)-$(CUSTOM_BUILD_DATE)
 CUSTOM_VERSION_PROP := fifteen
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.custom.build.date=$(BUILD_DATE) \
+    ro.custom.build.version=$(CUSTOM_DISPLAY_VERSION) \
     ro.custom.device=$(CUSTOM_BUILD) \
     ro.custom.fingerprint=$(ROM_FINGERPRINT) \
-    ro.custom.version=$(CUSTOM_VERSION) \
-    ro.modversion=$(CUSTOM_VERSION)
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.custom.build.date=$(BUILD_DATE) \
-    ro.custom.device=$(CUSTOM_BUILD) \
-    ro.custom.fingerprint=$(ROM_FINGERPRINT) \
+    ro.custom.releasetype=$(CUSTOM_BUILDTYPE) \
     ro.custom.version=$(CUSTOM_VERSION) \
     ro.modversion=$(CUSTOM_VERSION)
 

ashoss/tpp_vendor_aosp

config/version.mk
@@ -7,6 +7,7 @@ CUSTOM_BUILD_DATE_UTC := $(shell date -d '$(CUSTOM_DATE_YEAR)-$(CUSTOM_DATE_MONT
 CUSTOM_BUILD_DATE := $(CUSTOM_DATE_YEAR)$(CUSTOM_DATE_MONTH)$(CUSTOM_DATE_DAY)-$(CUSTOM_DATE_HOUR)$(CUSTOM_DATE_MINUTE)
 
 CUSTOM_PLATFORM_VERSION := 15.0
+CUSTOM_DISPLAY_VERSION := 1.0
 
 # Guidelines for setting tpp Build ID:
 #  - VR1  = V Release 1
@@ -19,15 +20,22 @@ CUSTOM_PLATFORM_VERSION := 15.0
 
 CUSTOM_BUILD_ID := VR1.35.DEV.FEB.BETA01
 
-CUSTOM_VERSION := tpp_$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)
+CUSTOM_BUILDTYPE ?= UNOFFICIAL
+
+CUSTOM_VERSION := tpp_$(CUSTOM_DISPLAY_VERSION)-$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILDTYPE)-$(CUSTOM_BUILD_DATE)
 CUSTOM_VERSION_PROP := fifteen
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.custom.build.date=$(BUILD_DATE) \
+    ro.custom.build.version=$(CUSTOM_DISPLAY_VERSION) \
     ro.custom.device=$(CUSTOM_BUILD) \
     ro.custom.fingerprint=$(ROM_FINGERPRINT) \
+    ro.custom.releasetype=$(CUSTOM_BUILDTYPE) \
     ro.custom.version=$(CUSTOM_VERSION) \
     ro.modversion=$(CUSTOM_VERSION)
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.custom.build.id=$(CUSTOM_BUILD_ID)
 
 # Signing
 -include vendor/lineage-priv/keys/keys.mk