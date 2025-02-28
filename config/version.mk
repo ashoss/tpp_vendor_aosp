aospb-staging/vendor_aosp

vendor: introduce a new shell variable for incremental version
Change-Id: Id276a753f07f51fc0d69872df3bf92aca440fb5d

config/version.mk
@@ -10,13 +10,15 @@ ifeq ($(IS_SIGNED),true)
 -include vendor/lineage-priv/keys/keys.mk
 endif
 
-CUSTOM_PLATFORM_VERSION := 15.0
+CUSTOM_INCREMENTAL := .1
+
+CUSTOM_PLATFORM_VERSION := 15
 
 CUSTOM_BUILDTYPE ?= UNOFFICIAL
 
 CUSTOM_SHOW_VERSION := $(CUSTOM_BUILD)-$(CUSTOM_BUILD_DATE)
 
-CUSTOM_VERSION := aosPB_$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD)-$(CUSTOM_BUILDTYPE)-$(CUSTOM_BUILD_DATE)$(if $(IS_SIGNED),-signed)
+CUSTOM_VERSION := aosPB_$(CUSTOM_PLATFORM_VERSION)$(CUSTOM_INCREMENTAL)-$(CUSTOM_BUILD)-$(CUSTOM_BUILDTYPE)-$(CUSTOM_BUILD_DATE)$(if $(IS_SIGNED),-signed)
 CUSTOM_VERSION_PROP := fifteen
 
 PRODUCT_SYSTEM_PROPERTIES += \

ashoss/tpp_vendor_aosp

config/version.mk
@@ -10,14 +10,15 @@ ifeq ($(IS_SIGNED),true)
 -include vendor/lineage-priv/keys/keys.mk
 endif
 
-CUSTOM_PLATFORM_VERSION := 15.0
-CUSTOM_DISPLAY_VERSION := 1.0
+CUSTOM_INCREMENTAL := .1
+
+CUSTOM_PLATFORM_VERSION := 15
 
 CUSTOM_BUILDTYPE ?= UNOFFICIAL
 
 CUSTOM_SHOW_VERSION := $(CUSTOM_BUILD)-$(CUSTOM_DISPLAY_VERSION)-$(CUSTOM_BUILD_DATE)
 
-CUSTOM_VERSION := tpp_$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD)-$(CUSTOM_BUILDTYPE)-$(CUSTOM_BUILD_DATE)$(if $(IS_SIGNED),-signed)
+CUSTOM_VERSION := tpp_$(CUSTOM_PLATFORM_VERSION)$(CUSTOM_INCREMENTAL)-$(CUSTOM_BUILD)-$(CUSTOM_BUILDTYPE)-$(CUSTOM_BUILD_DATE)$(if $(IS_SIGNED),-signed)
 CUSTOM_VERSION_PROP := fifteen
 
 PRODUCT_SYSTEM_PROPERTIES += \