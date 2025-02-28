ProjectPixelage/android_vendor_lineage

vendor: Allow setting maintainer string with a flag

config/version.mk
@@ -19,6 +19,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.pixelage.build.version=$(PIXELAGE_DISPLAY_VERSION) \
     ro.pixelage.device=$(PIXELAGE_BUILD) \
     ro.pixelage.fingerprint=$(ROM_FINGERPRINT) \
+    ro.pixelage.maintainer=$(PIXELAGE_MAINTAINER) \
     ro.pixelage.releasetype=$(PIXELAGE_BUILDTYPE) \
     ro.pixelage.version=$(PIXELAGE_VERSION) \
     ro.modversion=$(PIXELAGE_VERSION)

ashoss/tpp_vendor_aosp

Change-Id: Id95456f04ac051d1797836a1e593bc55f6966b3d

config/version.mk
@@ -26,6 +26,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.custom.build.version=$(CUSTOM_DISPLAY_VERSION) \
     ro.custom.device=$(CUSTOM_BUILD) \
     ro.custom.fingerprint=$(ROM_FINGERPRINT) \
+    ro.custom.maintainer=$(CUSTOM_MAINTAINER) \
     ro.custom.releasetype=$(CUSTOM_BUILDTYPE) \
     ro.custom.showversion=$(CUSTOM_SHOW_VERSION) \
     ro.custom.version=$(CUSTOM_VERSION) \