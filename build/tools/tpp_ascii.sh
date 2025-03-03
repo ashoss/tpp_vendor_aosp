4cf6d4455b187d91177c1f704ed487446df3c356
MODEDGES <106741581+MODEDGES@users.noreply.github.com>
2025-03-03 14:22:09 -0800

    tools: udate our ascii art
    now we are looking cleaner

--- build/tools/tpp_ascii.sh
+++ build/tools/tpp_ascii.sh
@@ -5,15 +5,14 @@ RED='\033[0;31m'       # Red color
 YELLOW='\033[0;33m'    # Yellow color
 ENDCOLOR='\033[0m'     # Reset color to default
 
-# Output the colored banner with the updated text "TPP"
-echo -e "${RED}========================================================================${ENDCOLOR}"
-echo -e "${YELLOW}                           _______ _____    _____                    ${ENDCOLOR}"
-echo -e "${YELLOW}                          |__   __|  __ \  |  __ \                   ${ENDCOLOR}"
-echo -e "${YELLOW}                             | |  | |__) | | |__) |                  ${ENDCOLOR}"
-echo -e "${YELLOW}                             | |  |  ___/  |  ___/                   ${ENDCOLOR}"
-echo -e "${YELLOW}                             | |  | |      | |                       ${ENDCOLOR}"
-echo -e "${YELLOW}                             |_|  |_|      |_|                       ${ENDCOLOR}"
-echo -e "${YELLOW}                                                                     ${ENDCOLOR}"
-echo -e "${RED}========================================================================${ENDCOLOR}"
+# Output the colored banner with the updated text "The Pixel Project"
+echo -e "${RED}====================================================================================================${ENDCOLOR}"
+echo -e "${YELLOW} _____ _   _  _____  ______ _______   __ _____ _      ____________ _____   ___ _____ _____ _____ ${ENDCOLOR}"
+echo -e "${YELLOW}|_   _| | | ||  ___| | ___ \_   _\ \ / /|  ___| |     | ___ \ ___ \  _  | |_  |  ___/  __ \_   _|${ENDCOLOR}"
+echo -e "${YELLOW}  | | | |_| || |__   | |_/ / | |  \ V / | |__ | |     | |_/ / |_/ / | | |   | | |__ | /  \/ | |  ${ENDCOLOR}"
+echo -e "${YELLOW}  | | |  _  ||  __|  |  __/  | |  /   \ |  __|| |     |  __/|    /| | | |   | |  __|| |     | |  ${ENDCOLOR}"
+echo -e "${YELLOW}  | | | | | || |___  | |    _| |_/ /^\ \| |___| |____ | |   | |\ \\ \_/ /\__/ / |___| \__/\ | |  ${ENDCOLOR}"
+echo -e "${YELLOW}  \_/ \_| |_/\____/  \_|    \___/\/   \/\____/\_____/ \_|   \_| \_|\___/\____/\____/ \____/ \_/  ${ENDCOLOR}"
+echo -e "${RED}====================================================================================================${ENDCOLOR}"
 
 
